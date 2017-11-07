f9e2f58944cc0dbb389577b1b2d2151d5f3181e4
Ryan Slama <rms427@cornell.edu>
2017-11-06 22:07:40 -0500

    Fixed Spacing

--- images.py
+++ images.py
@@ -1,7 +1,7 @@
 import pygame
 
-wizard= pygame.image.load('images/wizard.png')
-wizard_left= pygame.image.load('images/wizard_left.png')
-tile= pygame.image.load('images/tile.png')
-campfire= pygame.image.load('images/campfire.png')
-campfire_2= pygame.image.load('images/campfire2.png')
+wizard = pygame.image.load('images/wizard.png')
+wizard_left = pygame.image.load('images/wizard_left.png')
+tile = pygame.image.load('images/tile.png')
+campfire = pygame.image.load('images/campfire.png')
+campfire_2 = pygame.image.load('images/campfire2.png')
